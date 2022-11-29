c64e75d7fe107fab14e51a94fb45ebeb6c9d38b5
jcdavidarg <jcdavidarg@gmail.com>
2022-11-29 02:29:38 -0300

    componente BotonClear modificado

--- src/componentes/BotonClear.js
+++ src/componentes/BotonClear.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../hojas-de-estilo/BotonClear.css";
 
-const BotonClear = (props) => {
+const BotonClear = () => {
   return <div className="boton-clear">Clear</div>;
 };
 
