567c3919138e51ff19c2a51df2b22f934d66a163
jcdavidarg <jcdavidarg@gmail.com>
2022-11-28 23:24:25 -0300

    componente App modificada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import freeCodeCampLogo from "../src/imagenes/freecode.png";
 
 function App() {
-  return <div className="App">Hola</div>;
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="Logo de freeCodeCamp"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
