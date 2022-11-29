10d4bbd30e050604f0c808db6120dc2c6011d704
jcdavidarg <jcdavidarg@gmail.com>
2022-11-29 00:37:49 -0300

    componente Boton creada

---
+++ src/componentes/Boton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../hojas-de-estilo/Boton.css";
+
+const Boton = (props) => {
+  const esOperador = (valor) => {
+    return isNaN(valor) && valor != "." && valor != "=";
+  };
+
+  return (
+    <div
+      className={`boton-contenedor ${
+        esOperador(props.children) ? "operador" : null
+      }`.trimEnd()}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Boton;
